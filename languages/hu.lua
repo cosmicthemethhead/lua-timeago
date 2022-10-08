820614ef35732beea9f6d30117b07804197b2dd4
cosmicthemethhead <thomasbohrer5@gmail.com>
2022-10-09 01:26:12 +0200

    feat(translations): added a hungarian one

---
+++ languages/hu.lua
@@ -0,0 +1,9 @@
+return {
+    justnow = 'csak most',
+    minute = {singular = 'egy perccel ezelőtt', plural = 'perccel ezelőtt'},
+    hour = {singular = 'egy órája', plural = 'órákkal ezelőtt'},
+    day = {singular = 'egy nappal ezelőtt', plural = 'napokkal ezelőtt'},
+    week = {singular = 'egy héttel ezelőtt', plural = 'héttel ezelőtt'},
+    month = {singular = 'egy hónappal ezelőtt', plural = 'hónappal ezelőtt'},
+    year = {singular = 'egy évvel ezelőtt', plural = 'évvel ezelőtt'}
+}
